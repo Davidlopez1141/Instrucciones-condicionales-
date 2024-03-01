d6205460e1bde9e4a7b3534ec15210b286d36e5e
davidlopez1141 <davidlopez@colegioguanenta.edu.co>
2024-03-01 08:24:49 -0500

    se realizo analisis y diseño

---
+++ ejercicio1cordenadas/ejercicio1_condicionales.py
@@ -0,0 +1,23 @@
+# Programa para saber en que lugar del plano cartesiano esta X o Y.
+
+# Input
+X = int(input("ingrese la coordenada x: "))
+Y = int(input("ingrese la coordenada y: "))
+
+# Processing
+if X == 0:
+    if Y == 0:
+        print("La coordenada" , (X,Y) , "esta en el origen")
+    else:
+        print("La coordenada" , (X,Y) , "esta en el eje Y")
+elif Y == 0:
+    print("La coordenada" , (X,Y) , "esta en el eje X")
+elif X > 0:
+    if Y > 0:
+        print("La coordenada" , (X,Y) , "estas en el cuadrante 1")
+    else:
+        print("La coordenada" , (X,Y) , "estas en el cuadrante 4")
+elif Y < 0:
+    print("La coordenada" , (X,Y) , "estas en el cuadrante 3")
+else:
+    print("La coordenada" , (X,Y) , "estas en el cuadrante 2") 
